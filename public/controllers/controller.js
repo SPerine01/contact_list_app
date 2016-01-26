c0dc563aee19bb02fb299054915253b294c2b926
Sumayyah Perine <sperine01@gmail.com>
2016-01-26 00:47:58 -0500

    retrieve data from controller and server

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,4 +1,11 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-    console.log('Hello World from controller');
-}]);
+    console.log("Hello World from controller");
+
+    $http.get("/contactlist").success(function(response) {
+    	console.log("Got my requested data");
+    	$scope.controller = response;
+    });
+
+    
+}]);﻿
--- server.js
+++ server.js
@@ -3,5 +3,30 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get("/contactlist", function (req, res) {
+	console.log("This is my GET request")
+
+	person1= {
+        name: "May",
+        email: "may@gmail.com",
+        number: "(347)888-8881"
+    };
+
+    person2 = {
+        name:"Chris",
+        email:"chris@gmail.com",
+        number: "(347)888-8882"
+    };
+
+    person3={
+        name: "Bobby",
+        email:"bobby@gmail.com",
+        number: "(347)888-8883"
+    };
+
+	var contactlist = [person1, person2, person3];
+	res.json(contactlist);
+});
+
 app.listen(2016);
 console.log("Server running on port 2016");
